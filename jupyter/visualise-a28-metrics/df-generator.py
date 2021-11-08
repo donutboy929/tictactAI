fcdab22e5f81464f228c3f0bfe674199303ae230
Daniel Ng <daniel.ng@area28.io>
2021-11-08 14:09:46 +0800

    feat: add binomial distribution to active times of users

--- jupyter/visualise-a28-metrics/df-generator.py
+++ jupyter/visualise-a28-metrics/df-generator.py
@@ -32,42 +32,45 @@ def random_date(start, end, prop):
 header = ['id', 'type', 'userId', 'sessionId', 'sceneId', 'timestamp', 'data']
 data_csv = list()
 id = 0
-timestamp = random_date(
-    "2020-01-01 01:30:00", "2024-12-01 05:45:44",  random.random())
+timestamp = datetime.datetime(2020, 1, 2, 9, 0, 0, 0)
+# random_date("2020-01-01 01:30:00", "2024-12-01 05:45:44",  random.random())
 
-for i in range(100):
-    events_this_sesh = random.randint(2, 3)
-    userId = random.randrange(10)
-    sessionId = i
+for i in range(7): # number of sessions
     sceneId = random.randrange(20)
-    timestamp = datetime.datetime.strptime(
-        str(timestamp), '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=int(rnd.binomial(n=7, p=0.2, size=1)[0]), minutes=random.randrange(59), seconds=random.randrange(59))
-    data = {}
-    
-    for j in range(events_this_sesh):
-        id = id + 1
-        type = ''
+    if True: #i % 10 == 0: 
+        timestamp = datetime.datetime.strptime(
+            str(timestamp), '%Y-%m-%d %H:%M:%S') + datetime.timedelta(days=1)
+    for j in range(10): # userid
+        events_this_sesh = random.randint(2, 3) # randomly choose if this user will rate or not
+        userId = j
+        sessionId = i
+        this_timestamp = datetime.datetime.strptime(
+            str(timestamp), '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=int(rnd.binomial(n=4, p=0.4, size=1)[0]), minutes=random.randrange(59), seconds=random.randrange(59))
         data = {}
-        if j == 0:
-            type = 'CORE_SCENE_JOIN' 
-            data['rating'] = None
-        elif j == 1:
-            type = 'CORE_SCENE_LEAVE'
-            data['rating'] = None
-        elif j==2:
-            type = 'CORE_SCENE_RATE'
-            data['rating'] = rnd.binomial(n=5, p=0.6, size=1)[0]
+        
+        for k in range(events_this_sesh):
+            id = id + 1
+            type = ''
+            data = {}
+            if k == 0:
+                type = 'CORE_SCENE_JOIN' 
+                data['rating'] = None
+            elif k == 1:
+                type = 'CORE_SCENE_LEAVE'
+                data['rating'] = None
+            elif k==2:
+                type = 'CORE_SCENE_RATE'
+                data['rating'] = rnd.binomial(n=5, p=0.65, size=1)[0]
 
-        if j == 1:
-            timestamp = datetime.datetime.strptime(
-                str(timestamp), '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=random.randrange(7), minutes=random.randrange(59), seconds=random.randrange(59))
-        elif j == 2:
-            timestamp = datetime.datetime.strptime(
-                str(timestamp), '%Y-%m-%d %H:%M:%S') + datetime.timedelta(minutes=random.randrange(2), seconds=random.randrange(59))
+            if k == 1:
+                this_timestamp = datetime.datetime.strptime(
+                    str(this_timestamp), '%Y-%m-%d %H:%M:%S') + datetime.timedelta(hours=int(rnd.binomial(n=7, p=0.3, size=1)[0]), minutes=random.randrange(59), seconds=random.randrange(59))
+            elif k == 2:
+                this_timestamp = datetime.datetime.strptime(
+                    str(this_timestamp), '%Y-%m-%d %H:%M:%S') + datetime.timedelta(minutes=random.randrange(2), seconds=random.randrange(59))
 
-        data_csv.append([id, type, userId, sessionId, sceneId, datetime.datetime.strptime(
-            str(timestamp), '%Y-%m-%d %H:%M:%S'), data])
-
+            data_csv.append([id, type, userId, sessionId, sceneId, datetime.datetime.strptime(
+                str(this_timestamp), '%Y-%m-%d %H:%M:%S'), data])
     
 
 with open('dummy.csv', 'w', encoding='UTF8', newline='') as f:
